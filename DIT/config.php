b16a1fec294dcc22120959e5187766f272fb5e87
Dulanka Ediriweera <160330250+DulankaEdiriweera@users.noreply.github.com>
2024-12-11 17:02:20 +0530

    Update config.php

--- DIT/config.php
+++ DIT/config.php
@@ -1,15 +1,1 @@
-<?php
-	$servername = "localhost";
-	$username = "root";
-	$password = "PWD@#123dula";
-	$database = "dogboardingdb";
 
-	// Create connection
-	$conn = new mysqli($servername, $username, $password, $database);
-
-	// Check connection
-	if ($conn->connect_error) {
-		die("Connection failed: " . $conn->connect_error);
-	}
-	//echo "Connected successfully";
-?>
